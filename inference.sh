c3ebe0a65e9f2304ae955c479589408c9088d538
YIYANGCAI <caiyiyang980311@icloud.com>
2026-03-25 11:52:12 +0800

    test code

--- inference.sh
+++ inference.sh
@@ -1,5 +1,6 @@
+# inference code for long video generation
 torchrun \
-  --nproc_per_node=1 \
+  --nproc_per_node=8 \
   --master_port=29500 \
   inference.py \
   --config_path configs/longlive_inference.yaml
